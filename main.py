a00a22d3d994f5254ebb2e3663a1507c0b8712f4
theJoseNeto <joseneto180201@gmail.com>
2021-12-18 12:41:14 -0300

    .

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import os
 import dotenv 
 from src.Telegram_bot import Telegram_bot
 
-dotenv.load_dotenv( dotenv.find_dotenv())
-token = os.getenv("TELEGRAM_URL_CONNECTION")
-url_base = f"https://api.telegram.org/bot{token}"
 
-bot1 = Telegram_bot(token, url_base)
-user_info = bot1.get_user_info()
-user_message = bot1.get_message()
-get_email = bot1.save_email()
+def run():
 
-print(user_info, " Mensagem: " ,user_message)
+    dotenv.load_dotenv( dotenv.find_dotenv())
+    token = os.getenv("TELEGRAM_URL_CONNECTION")
+    url_base = f"https://api.telegram.org/bot{token}"
+
+    bot1 = Telegram_bot(token, url_base)
+    user_info = bot1.get_user_info()
+    user_message = bot1.get_message()
+    get_email = bot1.save_email()
+    print(user_info, " Mensagem: " ,user_message)
+
+run()
