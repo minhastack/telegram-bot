fec1c02993fb783cc14e73c225f16ac2313681f5
theJoseNeto <joseneto180201@gmail.com>
2022-01-12 15:47:22 -0300

    .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv( dotenv.find_dotenv())
 token = os.getenv("TELEGRAM_URL_CONNECTION")
 bot = Telegram_bot(token,'-1001387626450')
 
-while True:
-    bot.message_watcher()
-    time.sleep(2)
+# while True:
+#     bot.message_watcher()
+#     time.sleep(2)
 
