affa7b1b003759eadac93f34790590f79ff29acb
theJoseNeto <joseneto180201@gmail.com>
2022-01-19 11:15:07 -0300

    cleanup trash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from unittest import result
+from urllib import request 
 import dotenv 
 import time
 from src.Telegram_bot import Telegram_bot
@@ -6,33 +8,9 @@ from src.Telegram_bot import Telegram_bot
 dotenv.load_dotenv( dotenv.find_dotenv())
 token = os.getenv("TELEGRAM_URL_CONNECTION")
 
-bot = Telegram_bot(token,'-1001592206421')
+bot = Telegram_bot(token,'-1001387626450')
 
 while True:
     bot.message_watcher()
     time.sleep(2)
-
-# teste = {
-#     "update_id": 277257923,
-#      "message": {
-#         "message_id": 566,
-#      "from": {
-#         "id": 1932640952,
-#      "is_bot": False,
-#      "first_name": "Descolado",
-#      "language_code": "en"},
-#  "chat": {
-#     "id": -1001592206421,
-#      "title": "teste",
-#      "type": "supergroup"},
-#  "date": 1642594899,
-# "new_chat_title": "teste"
-# }
-# }
-
-# if "new_chat_title" in teste["message"]:
-#     print("não é uma mensagem")
-# else:
-#     print("É uma mensagem")
-
 
